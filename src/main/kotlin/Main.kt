55bc7b69e89f8a80e9f1b2559c4145646b1474d8
garyma <garymar2007@gmail.com>
2024-08-19 17:07:15 +0200

    added jaxb lib.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -12,31 +12,33 @@ import javax.xml.transform.stream.StreamSource
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-   try {
-      val date: LocalDate = LocalDate.parse(("2024-13-01"))
-      println("Date: $date")
-   } catch (e: DateTimeParseException) {
-      println("Unbale to parse. ${e.message}")
-   }
+//   try {
+//      val date: LocalDate = LocalDate.parse(("2024-13-01"))
+//      println("Date: $date")
+//   } catch (e: DateTimeParseException) {
+//      println("Unbale to parse. ${e.message}")
+//   }
+//
+//   val result: Result<LocalDate> = runCatching { LocalDate.parse("2024-13-01") }
+//
+//   result.onSuccess { date -> println("Day of the Week: ${date.dayOfWeek}") }
+//   result.onFailure { e -> println("Unable to parse. ${e.message}") }
+//
+//   val listOfStrings: List<String> = listOf("one", "two", "three")
+//    println("Second item: ${secondItemOf(listOfStrings)}")
+//
+//   val listOfInts: List<Int> = listOf(1, 2, 3)
+//    println("Second item: ${secondItemOf(listOfInts)}")
+//
+//   val list: List<Any> = listOf(1, "two", 3)
+//    println("Second item has type String: ${secondItemHasType<String>(list)}")
+//    println("Second item has type Int: ${secondItemHasType<Int>(list)}")
+//
+//    val jaxbContext = JAXBContext.newInstance(Product::class.java)
+//    val unmarshaller = jaxbContext.createUnmarshaller()
+//    val product: Product = unmarshaller.unmarshal("src/main/resources/product.xml")
 
-   val result: Result<LocalDate> = runCatching { LocalDate.parse("2024-13-01") }
-
-   result.onSuccess { date -> println("Day of the Week: ${date.dayOfWeek}") }
-   result.onFailure { e -> println("Unable to parse. ${e.message}") }
-
-   val listOfStrings: List<String> = listOf("one", "two", "three")
-    println("Second item: ${secondItemOf(listOfStrings)}")
-
-   val listOfInts: List<Int> = listOf(1, 2, 3)
-    println("Second item: ${secondItemOf(listOfInts)}")
-
-   val list: List<Any> = listOf(1, "two", 3)
-    println("Second item has type String: ${secondItemHasType<String>(list)}")
-    println("Second item has type Int: ${secondItemHasType<Int>(list)}")
-
-    val jaxbContext = JAXBContext.newInstance(Product::class.java)
-    val unmarshaller = jaxbContext.createUnmarshaller()
-    val product: Product = unmarshaller.unmarshal("src/main/resources/product.xml")
+    inlineFunction { println("Hello, Kotlin!") }
 }
 
 fun <T> secondItemOf(list: List<T>): T {
@@ -57,3 +59,10 @@ data class Product @JvmOverloads constructor(
     @XmlElement var name: String = "",
     @XmlElement var price: Int = 0
 )
+
+//higher order function
+inline fun inlineFunction(block: () -> Unit) {
+    println("Before calling block")
+    block()
+    println("After calling block")
+}
